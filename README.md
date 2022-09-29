0b00f10ef92956ebae228d122db05745768219c5
Lautaro <lu4ult@gmail.com>
2022-09-28 23:47:38 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # feriados-argentina
+
+Este pequeño script busca obtener el listado de días feriados de Argentina desde la API de https://nolaborables.com.ar
+para luego filtrar los datos que me interesan y reducir los caracteres de los índices, ya que el resultado será consumido por un microcontrolador ESP8266 el cuál posee memoria limitada y el uso de la API ya citada generaba fragmentación del HEAP.
+
+Además, puedo de esta forma agregar feriados que no fueron agregados durante este año, como el del Censo en Mayo.
+
---
+++ scrypt.py
@@ -0,0 +1,34 @@
+import json, requests
+
+ano = "2022"
+
+rtta = requests.get('https://nolaborables.com.ar/api/v2/feriados/'+ano)
+#print(type(rtta))
+
+#rtta = json.dumps(rtta).replace("\\n","</br>")
+
+no_laborables_payload = rtta.json()
+
+#print(len(no_laborables_payload))
+#print(no_laborables_payload)
+
+file_name = "feriados"+ano+".json"
+cont = 0
+with open(file_name, 'w') as f:
+    f.write("[")
+    for i in no_laborables_payload:
+        cont += 1
+        linea = str("\n" + r"{" + f"\"mtv\":\"{i['motivo']}\",\"ms\":{i['mes']},\"d\":{i['dia']},\"nf\":\"{i['info']}\"" + r"}")
+        if cont != len(no_laborables_payload):
+            linea += ","
+        linea = linea.replace("https://es.wikipedia.org/wiki/","")
+        linea = linea.replace("ñ","&ntilde")
+        linea = linea.replace("á","a")
+        linea = linea.replace("é","e")
+        linea = linea.replace("í","&iacute")
+        linea = linea.replace("ó","o")
+        linea = linea.replace("ú","u")
+        linea = linea.replace("ü","u")
+        f.write(linea)
+    f.write("]")
+    f.close()
